e8050bdb36e12e79db76d01a50a2eaf1d97b7cb7
Markus Damm <nullachtfuffzn@arcor.de>
2016-10-12 22:47:27 +0200

    display items in a list in MainActivity

--- app/src/main/java/de/markusdamm/financesharer/MainActivity.java
+++ app/src/main/java/de/markusdamm/financesharer/MainActivity.java
@@ -2,12 +2,36 @@ package de.markusdamm.financesharer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    List<String> financersList = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeViews();
+        fillList();
+    }
+
+    private void initializeViews() {
+        listView = (ListView) findViewById(R.id.lvFinances);
+    }
+
+    private void fillList(){
+        financersList.add("abc");
+        financersList.add("def");
+        ListAdapter listAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,financersList);
+        listView.setAdapter(listAdapter);
     }
 }
