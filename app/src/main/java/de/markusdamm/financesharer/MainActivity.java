0e74ff9aa6b6207da7702dba5eccfa732daeeaf8
Markus Damm <nullachtfuffzn@arcor.de>
2016-10-12 23:04:28 +0200

    add OnClickListener

--- app/src/main/java/de/markusdamm/financesharer/MainActivity.java
+++ app/src/main/java/de/markusdamm/financesharer/MainActivity.java
@@ -1,7 +1,10 @@
 package de.markusdamm.financesharer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -22,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
         initializeViews();
         fillList();
+        addListenerOnListViewItemSelection();
     }
 
     private void initializeViews() {
@@ -33,5 +37,20 @@ public class MainActivity extends AppCompatActivity {
         financersList.add("def");
         ListAdapter listAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,financersList);
         listView.setAdapter(listAdapter);
+    }
+
+    public void addListenerOnListViewItemSelection() {
+        listView.setClickable(true);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = listView.getItemAtPosition(position).toString();
+                Intent intent = new Intent(view.getContext(), SingleFinance.class);
+                intent.putExtra("singleFinanceName", item);
+                startActivity(intent);
+            }
+        });
+
     }
 }
